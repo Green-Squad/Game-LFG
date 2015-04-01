7ca317f4ae065feef2fcab5c89a5946112264b8c
Prelang Builder <builder@prelang.com>
2015-04-01 14:32:21 +0000

    Installed relation 'belongs_to' on model 'User'

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :trackable, :validatable
   belongs_to :identifier
+  belongs_to :player
 end
