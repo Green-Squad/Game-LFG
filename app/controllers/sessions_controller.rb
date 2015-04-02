751720b7e8d30d5c304833ba32e1739737011552
Jordan Smith <jordan.r.smith@gmail.com>
2015-04-02 10:18:23 -0400

    Update User model to allow email and password to be blank.  Include Devise test helpers with rspec. Initial work on sessions controller for logging in and creating a user/identifier.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,28 @@
 class SessionsController < ApplicationController
 
   def create
-    fingerprint = params[:fingerprint]
+    fingerprint = params[:fingerprint].present? ? params[:fingerprint] : nil
     ip_address = request.remote_ip
 
-    render json: {  }
+    if fingerprint
+      identifier = Identifier.where(fingerprint: fingerprint, ip_address: ip_address).first
+
+      if user = current_user
+        Identifier.create(fingerprint: fingerprint, ip_address: ip_address, user_id: user.id)
+      elsif identifier
+        user = User.find(identifier.user_id)
+      else
+        user = User.create
+        Identifier.create(fingerprint: fingerprint, ip_address: ip_address, user_id: user.id)
+      end
+
+      status = 201
+      json = user.to_json
+    else
+      status = 422
+      json = { errors: ['No fingerprint was sent.'] }
+    end
+
+    render json: json, status: status
   end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,22 +2,59 @@ require 'rails_helper'
 
 describe SessionsController, type: :controller do
   describe 'POST #create' do
-
     before(:each) do
-      @fingerprint = Faker::Number.number(10)
+      request.headers['Accept'] = 'application/json'
+    end
+    context 'good request' do
+
+      before(:each) do
+        @fingerprint = Faker::Number.number(10)
+      end
+
+      subject { post :create, {fingerprint: @fingerprint}, format: :json }
+
+      it 'creates a new user' do
+        expect { subject }.to change { User.count }.by(1)
+      end
+
+      it 'returns a user' do
+        subject
+        response_json = JSON.parse(response.body, symbolize_names: true)
+
+        expect(User.find(response_json[:id])).to_not eq(nil)
+      end
+
+      it 'returns status 201' do
+        subject
+        expect(response).to have_http_status('201')
+      end
+
     end
 
-    subject { post :create, { fingerprint: @fingerprint }.to_json, { accept: 'application/json' } }
+    context 'bad request' do
 
-    it 'creates a new user' do
-      expect{subject}.to change{User.count}.by(1)
-    end
+      before(:each) do
+        @fingerprint = ''
+      end
 
-    it 'returns a user' do
-      subject
-      response_json = JSON.parse(response.body, symbolize_names: true)
+      subject { post :create, {fingerprint: @fingerprint}, format: :json }
 
-      expect(User.find(response_json[:id])).to_not eq(nil)
+      it 'does not create a new user' do
+        expect { subject }.to change { User.count }.by(0)
+      end
+
+      it 'returns an error' do
+        subject
+        response_json = JSON.parse(response.body, symbolize_names: true)
+
+        expect(response_json[:errors]).to_not eq(nil)
+      end
+
+      it 'returns status 422' do
+        subject
+        expect(response).to have_http_status('422')
+      end
+
     end
 
   end
